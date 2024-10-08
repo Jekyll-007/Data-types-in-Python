f77606175b4e63276521c3519f0464a4a314a309
Jekyll-007 <teenofeejay@gmail.com>
2024-10-08 18:18:43 +0100

    few changes

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ is_student=True
 weight=56.5
 print("name: ",name)
 print("Dta type is: ",type(name))
+print("age: ",age)
 print("Dta type is: ",type(age))
+print("weight: ",weight)
 print("Dta type is: ",type(weight))
